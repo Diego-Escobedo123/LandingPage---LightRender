0e0585252fd35c350d70beb42b8d39d0d56b55e5
Diego-Escobedo123 <diego.escobedo@ufm.edu>
2026-04-19 11:20:07 -0600

    Business model component

--- light-render/src/app/app.ts
+++ light-render/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeroComponent } from './components/hero/hero';
 import { TechnologiesComponent } from './components/technologies/technologies';
 import { AudienceComponent } from './components/audience/audience';
+import { BusinessComponent } from './components/business/business';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroComponent, TechnologiesComponent, AudienceComponent],
+  imports: [HeroComponent, TechnologiesComponent, AudienceComponent, BusinessComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
