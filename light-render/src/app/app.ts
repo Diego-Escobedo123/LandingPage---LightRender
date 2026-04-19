71d9644a004c34fdc7d45da083ff5cbd81330be8
Diego-Escobedo123 <diego.escobedo@ufm.edu>
2026-04-18 22:50:24 -0600

    Technologies component added

--- light-render/src/app/app.ts
+++ light-render/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './components/hero/hero';
+import { TechnologiesComponent } from './components/technologies/technologies';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, TechnologiesComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
