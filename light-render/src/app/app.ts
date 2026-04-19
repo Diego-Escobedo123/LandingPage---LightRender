be5b97609bd5d6a00b7a2ef6968a8b826043a44c
Diego-Escobedo123 <diego.escobedo@ufm.edu>
2026-04-19 11:09:34 -0600

    Audience component content

--- light-render/src/app/app.ts
+++ light-render/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './components/hero/hero';
 import { TechnologiesComponent } from './components/technologies/technologies';
+import { AudienceComponent } from './components/audience/audience';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroComponent, TechnologiesComponent],
+  imports: [HeroComponent, TechnologiesComponent, AudienceComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ light-render/src/app/components/audience/audience.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audience',
+  standalone: true,
+  imports: [],
+  templateUrl: './audience.html',
+  styleUrl: './audience.css'
+})
+export class AudienceComponent {}
