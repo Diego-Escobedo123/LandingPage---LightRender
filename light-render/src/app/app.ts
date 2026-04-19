0a4bfbe35c6ed3d049f044c57f5b6d161140b9f8
Diego-Escobedo123 <diego.escobedo@ufm.edu>
2026-04-19 11:29:01 -0600

    Purpose component added

--- light-render/src/app/app.ts
+++ light-render/src/app/app.ts
@@ -3,11 +3,12 @@ import { HeroComponent } from './components/hero/hero';
 import { TechnologiesComponent } from './components/technologies/technologies';
 import { AudienceComponent } from './components/audience/audience';
 import { BusinessComponent } from './components/business/business';
+import { PurposeComponent } from './components/purpose/purpose';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroComponent, TechnologiesComponent, AudienceComponent, BusinessComponent],
+  imports: [HeroComponent, TechnologiesComponent, AudienceComponent, BusinessComponent, PurposeComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
